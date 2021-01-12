9e35c56b075dcea8fa2cc5cea4b7f6b19626931c
Soloman Garcia <soloman.garcia@gmail.com>
2021-01-12 10:38:50 -0700

    work on index.js

--- index.js
+++ index.js
@@ -2,12 +2,20 @@ const setupDiv = document.getElementById(setup);
 const punchlineDiv = document.getElementById(punchline);
 const punchlineBtn = document.getElementById(punchlineBtn);
 const newJokeBtn = document.getElementById("newJokeBtn");
+let punchline;
 
 async function getJoke() {
   const jokePromise = await fetch(
     "https://official-joke-api.appspot.com/jokes/programming/random"
   );
   const joke = await jokePromise.json();
+
+  setupDiv.innerHTML = joke[0].setup;
+
+  punchline = joke[0].punchline;
+
+  punchlineBtn.classList.toggle("hidden");
+  newJokeBtn.classList.toggle("hidden");
 }
 
 getJoke();
